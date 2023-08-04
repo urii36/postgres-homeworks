9a6fddb5c0cc167e7919b223343ca895a5c447e4
urii36 <kazan313131@gmail.com>
2023-08-04 12:59:32 +0300

    homework number 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar (100) NOT NULL,
+	last_name varchar (100) NOT NULL,
+	title varchar (100) NOT NULL,
+	birth_date DATE,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar (100) PRIMARY KEY,
+	company_name varchar (100) NOT NULL,
+	contact_name varchar (100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar REFERENCES customers (customer_id),
+	employee_id int REFERENCES employees (employee_id),
+	order_date DATE,
+	ship_city varchar (35)
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='140386')
+
+
+def intersection_to_db(file, query):
+    with open(file) as fp:
+        reader = csv.reader(fp)
+        next(reader)
+        with conn.cursor() as curs:
+            curs.executemany(query, reader)
+    conn.commit()
+
+
+query_customers = """INSERT INTO customers (customer_id, company_name, contact_name)
+                      VALUES (%s,%s,%s);"""
+query_employees = """INSERT INTO employees (employee_id,first_name,last_name,title,birth_date,notes)
+                      VALUES (%s,%s,%s,%s,%s,%s);"""
+query_orders = """INSERT INTO orders (order_id,customer_id,employee_id,order_date,ship_city)
+                      VALUES (%s,%s,%s,%s,%s);"""
+
+intersection_to_db('north_data/customers_data.csv', query_customers)
+intersection_to_db('north_data/employees_data.csv', query_employees)
+intersection_to_db('north_data/orders_data.csv', query_orders)
+conn.close()
